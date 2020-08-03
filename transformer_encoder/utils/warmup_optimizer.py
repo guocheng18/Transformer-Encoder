fcb5950162c8511eb38056449a462b4746ae562d
Cheng Guo <guochengrz@foxmail.com>
2020-08-03 09:24:56 +0800

    reformat code

--- transformer_encoder/utils/warmup_optimizer.py
+++ transformer_encoder/utils/warmup_optimizer.py
@@ -8,19 +8,18 @@ class WarmupOptimizer:
 
     def __init__(self, base_optimizer: optim.Optimizer, d_model: int, scale_factor: float, warmup_steps: int):
         self.base_optimizer = base_optimizer
-        self._step = 0
         self.warmup_steps = warmup_steps
         self.scale_factor = scale_factor
         self.d_model = d_model
+        self._step = 0
         self._rate = 0
 
     def step(self):
         """Update parameters and rate"""
         self._step += 1
-        rate = self.rate()
+        self._rate = self.rate()
         for p in self.base_optimizer.param_groups:
-            p["lr"] = rate
-        self._rate = rate
+            p["lr"] = self._rate
         self.base_optimizer.step()
 
     def zero_grad(self):
@@ -30,4 +29,4 @@ class WarmupOptimizer:
         """Implement `lrate` above"""
         if step is None:
             step = self._step
-        return self.scale_factor * (self.d_model ** (-0.5) * min(step ** (-0.5), step * self.warmup_steps ** (-1.5)))
+        return self.scale_factor * self.d_model ** (-0.5) * min(step ** (-0.5), step * self.warmup_steps ** (-1.5))
